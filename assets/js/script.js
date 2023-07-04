212dc921ec1402d295875315997e6c7f6e8f6fc7
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-04 18:58:26 -0500

    added the questions to java

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Quiz JavaScript
+
+// Questions
+
+var questions = [
+        {
+          question: "What is the purpose of CSS?",
+          options: [
+            "To style the content and layout of a web page",
+            "To define the functionality and behavior of a web page",
+            "To manipulate data in a database",
+            "To handle server-side operations"
+          ],
+          answer: "To style the content and layout of a web page"
+        },
+        {
+          question: "What is the output of the following JavaScript code?\n\nconsole.log(typeof 42);",
+          options: [
+            "number",
+            "string",
+            "boolean",
+            "undefined"
+          ],
+          answer: "number"
+        },
+        {
+          question: "Which of the following is NOT a valid programming language?",
+          options: [
+            "JavaScript",
+            "Python",
+            "HTML",
+            "Java"
+          ],
+          answer: "HTML"
+        },
+        {
+          question: "What is the result of the following code snippet?\n\nconsole.log(5 + '5');",
+          options: [
+            "55",
+            "10",
+            "NaN",
+            "Error"
+          ],
+          answer: "55"
+        },
+        {
+          question: "What does the acronym 'API' stand for?",
+          options: [
+            "Application Program Interface",
+            "Advanced Programming Interface",
+            "Automated Program Integration",
+            "Application Protocol Interface"
+          ],
+          answer: "Application Program Interface"
+        }
+      ];
+      
+
